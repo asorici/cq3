fbac7dcec4a80b75c4d8db0899fddee0b0ad573a
Mihai Trăscău <mihai.trascau@gmail.com>
2012-12-08 16:08:13 +0200

    Removed requiredObjects from CraftedObjects

--- framework/src/org/aimas/craftingquest/state/CraftedObject.java
+++ framework/src/org/aimas/craftingquest/state/CraftedObject.java
@@ -1,89 +1,69 @@
 package org.aimas.craftingquest.state;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+
+//import java.util.Set;
 
 /**
- * Description of a crafted object - value and means of construction. 
- * There are two types of crafted objects:
+ * Description of a crafted object - value and means of construction. There are
+ * two types of crafted objects:
  * <ul>
  * <li>simple objects - are constructed only from basic resources</li>
- * <li>complex objects - are constructed only from other objects </li>
+ * <li>complex objects - are constructed only from other objects</li>
  * </ul>
- *
+ * 
  */
 @SuppressWarnings("serial")
 public class CraftedObject implements Serializable {
 	public enum BasicResourceType implements Serializable {
-		R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12
+		WOOD, STONE, LEATHER, BRONZE, IRON, TITANIUM, GOLD
 	}
-	
+
 	public enum ObjectType implements Serializable {
-		O1, O2, O3, O4, O5, O6, O7, O8, O9, O10, O11, O12
+		SWORD, SHIELD, TOWER, TRAP
 	}
-	
+
 	/**
 	 * the object type
 	 */
 	private ObjectType type;
-	
+
 	/**
 	 * the object value
 	 */
 	private int value;
-	
+
 	/**
 	 * the object weight
 	 */
 	private int weight;
 	
 	/**
-	 * If not null, this field gives a list of alternative ways of constructing a complex object.<br/>
-	 * Each alternative is a mapping between <code>CraftedObject</code>s and their required quantities. 
-	 */
-	private List<HashMap<CraftedObject, Integer>> requiredObjects;
-	
-	/**
-	 * If not null, this field gives a list of alternative ways of constructing a simple object. <br/>
-	 * Each alternative is a mapping between <code>BasicResourceType</code>s and their required quantities.
+	 * If not null, this field gives a list of alternative ways of constructing
+	 * a simple object. <br/>
+	 * Each alternative is a mapping between <code>BasicResourceType</code>s and
+	 * their required quantities.
 	 */
 	private List<HashMap<BasicResourceType, Integer>> requiredResources;
-	
+
 	public CraftedObject() {
 	}
-	
-	public CraftedObject(ObjectType type, int value, List<HashMap<CraftedObject, Integer>> requiredObjects, List<HashMap<BasicResourceType, Integer>> requiredResources) {
+
+	public CraftedObject(ObjectType type, int value,
+			List<HashMap<CraftedObject, Integer>> requiredObjects,
+			List<HashMap<BasicResourceType, Integer>> requiredResources) {
 		this.type = type;
 		this.value = value;
-		this.requiredObjects = requiredObjects;
 		this.requiredResources = requiredResources;
-	}
-
-	public List<HashMap<CraftedObject, Integer>> getRequiredObjects() {
-		return requiredObjects;
 	}
 
 	public List<HashMap<BasicResourceType, Integer>> getRequiredResources() {
 		return requiredResources;
 	}
-		
-	
-	public List<List<CraftedObject>> getRequiredObjectsList() {
-		List<List<CraftedObject>> list = new ArrayList<List<CraftedObject>>();
-		
-		for (HashMap<CraftedObject, Integer> alternative : requiredObjects) {
-			Set<CraftedObject> keys = alternative.keySet();
-			ArrayList<CraftedObject> keysArray = new ArrayList<CraftedObject>(keys);
-			
-			list.add(keysArray);
-		}
-		
-		return list;
-	}
-	
+
 	public ObjectType getType() {
 		return type;
 	}
@@ -98,57 +78,46 @@ public class CraftedObject implements Serializable {
 	
 	@Override
 	public boolean equals(Object obj) {
-		if(!(obj instanceof CraftedObject)) {
+		if (!(obj instanceof CraftedObject)) {
 			return false;
 		}
-		
-		final CraftedObject other = (CraftedObject)obj;
-		
+
+		final CraftedObject other = (CraftedObject) obj;
+
 		if (this.type != other.type) {
 			return false;
 		}
-		
+
 		if (this.value != other.value) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return type.ordinal();
 	}
-	
+
 	@Override
 	public String toString() {
 		String info = "";
-		
+
 		info += "Obj: " + type.name() + "\n";
 		info += "    value: " + value + "\n";
-		
+
 		if (requiredResources != null && !requiredResources.isEmpty()) {
-			info += "    construction alternatives \n"; 
+			info += "    prerequisites \n";
 			for (HashMap<BasicResourceType, Integer> alternative : requiredResources) {
 				info += "        ";
 				for (BasicResourceType br : alternative.keySet()) {
-					info += br.name() + ":" + alternative.get(br) + " "; 
+					info += br.name() + ":" + alternative.get(br) + " ";
 				}
 				info += "\n";
 			}
 		}
-		
-		if (requiredObjects != null && !requiredObjects.isEmpty()) {
-			info += "    construction alternatives \n"; 
-			for (HashMap<CraftedObject, Integer> alternative : requiredObjects) {
-				info += "        ";
-				for (CraftedObject o : alternative.keySet()) {
-					info += o.type.name() + ":" + alternative.get(o) + " "; 
-				}
-				info += "\n";
-			}
-		}
-		
+
 		return info;
 	}
 }
