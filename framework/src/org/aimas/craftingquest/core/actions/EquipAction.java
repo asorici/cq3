6a33f01f822171d26ad6f49cd0950e23bb9b7976
Tudor Berariu <tudor.berariu@gmail.com>
2012-12-09 04:00:29 +0200

    Refactored EquipAction

--- framework/src/org/aimas/craftingquest/core/actions/EquipAction.java
+++ framework/src/org/aimas/craftingquest/core/actions/EquipAction.java
@@ -1,21 +1,14 @@
 package org.aimas.craftingquest.core.actions;
 
-import java.util.HashMap;
-import java.util.Iterator;
-
-import org.aimas.craftingquest.core.GamePolicy;
-import org.aimas.craftingquest.state.Blueprint;
 import org.aimas.craftingquest.state.GameState;
 import org.aimas.craftingquest.state.PlayerState;
 import org.aimas.craftingquest.state.Transition;
-import org.aimas.craftingquest.state.TransitionResult;
-import org.aimas.craftingquest.state.UnitState;
 import org.aimas.craftingquest.state.Transition.ActionType;
+import org.aimas.craftingquest.state.TransitionResult;
 import org.aimas.craftingquest.state.objects.ArmourObject;
-import org.aimas.craftingquest.state.objects.CraftedObject;
-import org.aimas.craftingquest.state.objects.EquippableObject;
+import org.aimas.craftingquest.state.objects.ICrafted;
+import org.aimas.craftingquest.state.objects.IEquippable;
 import org.aimas.craftingquest.state.objects.SwordObject;
-import org.aimas.craftingquest.state.objects.CraftedObject.BasicResourceType;
 
 public class EquipAction extends Action {
 	
@@ -26,12 +19,13 @@ public class EquipAction extends Action {
 	@Override
 	protected TransitionResult handle(GameState game, PlayerState player,
 			Transition transition) {
-		CraftedObject target = (CraftedObject)transition.operands[1];
+		ICrafted target = (ICrafted)transition.operands[1];
 
 		// check that the unit has the required resources/objects required for
 		// making the object
-		boolean carried = (playerUnit.carriedObjects.get(target) != null);
-		if (!carried) {
+		boolean isCarried = (playerUnit.carriedObjects.containsKey(target) &&
+				playerUnit.carriedObjects.get(target) > 0);
+		if (!isCarried) {
 			TransitionResult res = new TransitionResult(transition.id);
 			res.errorType = TransitionResult.TransitionError.EquipmentMissingError;
 			res.errorReason = "Player does not hold that equipment.";
@@ -57,11 +51,9 @@ public class EquipAction extends Action {
 
 	@Override
 	protected boolean validOperands(Transition transition) {
-		EquippableObject target = null;
-
+		IEquippable target = null;
 		try {
-			target = (EquippableObject)transition.operands[1];
-			
+			target = (IEquippable)transition.operands[1];
 			// target may not be null
 			if (target == null) {
 				return false;
@@ -70,7 +62,6 @@ public class EquipAction extends Action {
 		catch(ClassCastException ex) {
 				return false;
 		}
-		
 		return true;
 	}
 }
